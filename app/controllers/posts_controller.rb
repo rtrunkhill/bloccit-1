7761a735fda3d932805644dde9095b06ab0f781f
Elizabeth Fitzgerald <info@maggpress.com>
2017-01-06 14:29:53 -0500

    Checkpoint-20-Rails-More-CRUD completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
+
 
   def show
     @post = Post.find(params[:id])
@@ -20,12 +22,42 @@ class PostsController < ApplicationController
        flash[:notice] = "Post was saved."
        redirect_to @post
      else
-
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :new
      end
+
    end
 
-  def edit
-  end
-end
+
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+
+  def update
+       @post = Post.find(params[:id])
+       @post.title = params[:post][:title]
+       @post.body = params[:post][:body]
+
+       if @post.save
+         flash[:notice] = "Post was updated."
+         redirect_to @post
+       else
+         flash.now[:alert] = "There was an error saving the post. Please try again."
+         render :edit
+       end
+     end
+
+     def destroy
+     @post = Post.find(params[:id])
+
+ # #8
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
+   end
