bb290080bdefc0f33c1e48d3792e8bab2111c150
Elizabeth Fitzgerald <info@maggpress.com>
2017-01-25 18:50:37 -0500

    Assignment-31-Rails-Favorites

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,7 +3,6 @@ class FavoriteMailer < ApplicationMailer
 
   def new_comment(user, post, comment)
 
- # #18
      headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
      headers["References"] = "<post/#{post.id}@your-app-name.example>"
@@ -12,7 +11,16 @@ class FavoriteMailer < ApplicationMailer
      @post = post
      @comment = comment
 
- # #19
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+    def new_post(post)
+      headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+      headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+      @post = post
+
+      mail(to: post.user.email, subject: "You recently created post")
+      end
 end
